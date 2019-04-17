3e15440a86f209da605ee99735e8bfe9da511f3c
Leach <18202331361@163.com>
2019-04-17 10:52:24 +0800

    test commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=1;i<101;i++){
+             if(i%3 == 0 && i%5 != 0){
+                 System.out.println("Fizz");
+             }else if (i%3 != 0 && i%5 == 0){
+                 System.out.println("Buzz");
+             }else if (i%3 == 0 && i%5 == 0){
+                 System.out.println("FizzBuzz");
+             }else{
+                 System.out.println(i);
+             }
+        }
+        System.out.println("================================");
+        for (int i=1;i<101;i++){
+            if (i%3 == 0 && (i-3)%10== 0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("================================");
+        for (int i=1;i<101;i++){
+            if (i%3 != 0 && i%5 == 0 && (i-5)%10== 0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("================================");
+        for(int i=1;i<101;i++){
+            if(i%3 == 0 && i%5 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
