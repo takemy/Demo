943bf5c4ec877197d8d1f54d82291d766df3d128
狮子xiaoxiao <35675138+takemy@users.noreply.github.com>
2019-04-18 20:48:48 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
    //这是一段新代码
     public static  void  main(String args[]){
-        System.out.println("==========测试直接push");
-
+        System.out.println("==========测试直接push====失败");
+        System.out.println("==========可直接从源分支pull====");
     }
 }
