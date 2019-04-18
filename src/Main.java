1b9a490fa240ba46f35cc26e4311cc265a66104b
takemy <18202331361@163.com>
2019-04-18 20:33:47 +0800

    测试提交

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        //测试题完成
         for(int i=1;i<101;i++){
              if(i%3 == 0 && i%5 != 0){
                  System.out.println("Fizz");
